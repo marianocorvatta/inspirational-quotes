0d71ab8b059a2939c4cd0692104c587dc8e10d75
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-02-20 19:06:42 +0100

    Update application.tsx

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -20,19 +20,30 @@ export const fetchQuotes = async (count: number) => {
 };
 
 const Application = () => {
-  const [quote, setQuote] = useState();
+  // const [quote, setQuote] = useState<Quote | undefined>();
+  const [quotes, setQuotes] = useState<Quote[]>([]);
 
-  useEffect(() => {
-    fetchRandomQuote().then(setQuote);
-  }, []);
+  // useEffect(() => {
+  //   fetchRandomQuote().then(setQuote);
+  // }, []);
 
-  if (!quote) return <Loading />;
+  // if (!quote) return <Loading />;
   return (
     <main className="w-full max-w-2xl py-16 mx-auto">
       {/* <InspirationalQuote content={quote.content} source={quote.source} /> */}
-      {/* <Quotes>
-        <div className="grid grid-cols-2 gap-4"></div>
-      </Quotes> */}
+      <Quotes>
+        <div className="grid grid-cols-2 gap-4">
+          {quotes.map((quote) => {
+            return (
+              <InspirationalQuote
+                key={quote.id}
+                content={quote.content}
+                source={quote.source}
+              />
+            )
+          })}
+        </div>
+      </Quotes>
     </main>
   );
 };
